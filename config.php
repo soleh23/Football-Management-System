b69127658e1f625cef4086831a5b692f9700d5cf
MehmetTuranboy <mehmet.turanboy@gmail.com>
2018-04-28 17:49:45 +0300

    UI's added

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$hostname='dijkstra.ug.bcc.bilkent.edu.tr';
+$username='demir.topaktas';
+$password='1cbvndb';
+$dbname='demir_topaktas';
+
+// create connection
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+?>
