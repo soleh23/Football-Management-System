62d6f7778b5e25eb1bf87a36893338a6f11ba24a
ibrahimovshamil <ibrahimovsh95@gmail.com>
2018-04-30 00:48:23 +0300

    PLayer table added and User table is commented

--- Database Creation/src/FMSDatabase.java
+++ Database Creation/src/FMSDatabase.java
@@ -17,7 +17,7 @@ public class FMSDatabase {
 		try {
 			connection = DriverManager.getConnection(url, username, password);
 			System.out.println("Database connected!");
-		} 
+		}
 		catch (SQLException e) {
 			throw new IllegalStateException("Cannot connect the database!", e);
 		}
@@ -34,7 +34,7 @@ type int NOT NULL,
 UNIQUE (username)
 );*/
 		// dropping User table
-		try{
+		/*try{
 			System.out.println("Dropping User Table");
 			dropStatement = connection.createStatement();
 			String dropCustomer = "DROP TABLE User";
@@ -55,9 +55,9 @@ UNIQUE (username)
 			statement = connection.createStatement();
 			String user = "CREATE TABLE User " +
 					"(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
-					" username VARCHAR(25) NOT NULL, " + 
-					" password VARCHAR(25) NOT NULL, " + 
-					" type VARCHAR(10) NOT NULL, " + 
+					" username VARCHAR(25) NOT NULL, " +
+					" password VARCHAR(25) NOT NULL, " +
+					" type VARCHAR(10) NOT NULL, " +
 					" UNIQUE (username)) " +
 					" ENGINE InnoDB";
 			statement.executeUpdate(user);
@@ -67,7 +67,7 @@ UNIQUE (username)
 		}
 		System.out.println("User Table Created");
 
-		
+
 		// Updating Tables
 		PreparedStatement preparedStatement;
 
@@ -98,7 +98,85 @@ UNIQUE (username)
 			System.out.println(e);
 		}
 		System.out.println("Tuples into User table inserted");
-		
+
 	}
+	*/
+
+
+
+	try{
+		System.out.println("Dropping Player Table");
+		dropStatement = connection.createStatement();
+		String dropCustomer = "DROP TABLE Player";
+		dropStatement.executeUpdate(dropCustomer);
+	}
+	catch (Exception e){
+		System.out.println(e);
+	}
+	System.out.println("Player Table Dropped");
+
+
+	// creating tables
+	Statement statement;
+
+	// creating customer table
+	try{ 								// BUT IT DOES NOT HAVE ANY DEPENDECY WITH AGENT YET. JUST TRYING
+		System.out.println("Creating Player Table");
+		statement = connection.createStatement();
+		String user = "CREATE TABLE Player " +
+				"(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+				" username VARCHAR(25) NOT NULL, " +
+				" password VARCHAR(25) NOT NULL, " +
+				" type VARCHAR(10) NOT NULL, " +
+				" name VARCHAR(20) NOT NULL, " +
+				" surname VARCHAR(20) NOT NULL, " +
+				" age INT NOT NULL, " +
+				" salary INT NOT NULL, " +
+				" nationality VARCHAR(15) NOT NULL, " +
+				" birthdate DATE NOT NULL, " +
+				" UNIQUE (username), " +
+				" FOREIGN KEY(ID) REFERENCES User(Id))" +
+				" ENGINE InnoDB";
+		statement.executeUpdate(user);
+	}
+	catch (Exception e){
+		System.out.println(e);
+	}
+	System.out.println("Player Table Created");
+
+
+	// Updating Tables
+	PreparedStatement preparedStatement;
+
+	// insert tuples into customer
+	try{
+		System.out.println("Inserting tuples into Player table");
+		String sql;
+		sql = "INSERT INTO Player " +
+				"(username, password, type, name, surname, age, salary, nationality, birthdate)" +
+				"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+		String[][] tuples = new String[][]{{"kobramehmet", "iyibayramlar", "admin","Pakize", "Yildirim", "18", "100","Kurt", "2000-10-10"}};
+
+		for (int i = 0; i < tuples.length; i++){
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, tuples[i][0]);
+			preparedStatement.setString(2, tuples[i][1]);
+			preparedStatement.setString(3, tuples[i][2]);
+			preparedStatement.setString(4, tuples[i][3]);
+			preparedStatement.setString(5, tuples[i][4]);
+			preparedStatement.setString(6, tuples[i][5]);
+			preparedStatement.setString(7, tuples[i][6]);
+			preparedStatement.setString(8, tuples[i][7]);
+			preparedStatement.setString(9, tuples[i][8]);
+			preparedStatement.executeUpdate();
+		}
+	}
+	catch (Exception e){
+		System.out.println(e);
+	}
+	System.out.println("Tuples into Player table inserted");
+
+}
+
 }
 
